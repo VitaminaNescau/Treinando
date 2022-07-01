052fba1dd945b1f6c766e3ac62eab4f06825133f
VitaminaNescau <vitorjss82@gmail.com>
2022-07-01 00:02:40 -0300

    file sampe thread/exception/interface

---
+++ Java/sampleFullpratic.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class sampleFullpratic {
+    //nesse arquivo tentaria por em pratica thread/exception/inteface
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+}
