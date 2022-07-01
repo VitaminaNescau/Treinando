297d1d7c570f00b0f767b8728cdb3b78c3b193d1
VitaminaNescau <vitorjss82@gmail.com>
2022-06-30 23:57:41 -0300

    file java for sample a vscode for github

--- Java/vsGit.java
+++ Java/vsGit.java
@@ -1,6 +1,6 @@
 package Java;
 public class vsGit{
     public static void main(String[] args) {
-        System.out.println("Envia arquivo para o git hub");
+        System.out.println("Envia arquivo para o git hub e fazer commit");
     }
 }
