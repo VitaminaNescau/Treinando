9bec71f469e8b31e0ebbcbea45d9fad38f7f1072
VitaminaNescau <vitorjss82@gmail.com>
2022-07-05 02:04:08 -0300

    main

--- Java/relogio/relogio.java
+++ Java/relogio/relogio.java
@@ -1,6 +1,5 @@
 package Java.relogio;
 
-import java.sql.Time;
 import java.util.Calendar;
 
 public class relogio {
@@ -8,15 +7,17 @@ public class relogio {
     public static void main(String[] args) {
     horas clock = new horas();
     //clock.setTime();
-   while (true) {
-   try { System.out.println(clock.getTime());
-    
+   /*while (true) {
+    try { System.out.println(clock.getTime());
         Thread.sleep(1000);
     } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
+        //TODO Auto-generated catch block
+
         e.printStackTrace();
+
     }
-   }
-    
+
+}*/
+    clock.getTime();
  }   
 }
