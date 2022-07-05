e79045f475ca854921dd98c6c31c23f38aa4c3de
VitaminaNescau <vitorjss82@gmail.com>
2022-07-05 02:03:43 -0300

    metodos para visualizar a hora atual

--- Java/relogio/horas.java
+++ Java/relogio/horas.java
@@ -1,24 +1,39 @@
 package Java.relogio;
 
 import java.util.Calendar;
-import java.util.spi.CalendarDataProvider;
 
 public class horas {
     public int h,m,s;
-    public String hora;
+    public String hora,relogio;
     public Calendar time;
-
-    public String getTime(){
+    
+    public horas(){
+    
+    }
+    
+    
+    public String setTime(){
         time = Calendar.getInstance();
         h = time.get(Calendar.HOUR_OF_DAY);
         m = time.get(Calendar.MINUTE);
         s = time.get(Calendar.SECOND);
-        String teste = Integer.toString(h);
-        String relogio = (h+":"+m+":"+s);
+        relogio = (h+":"+m+":"+s);
         this.hora = relogio;
-        
         return hora;
     }
-   
+    public void getTime(){
+        horas clock = new horas();
+        while (true) {
+            try { System.out.println(clock.setTime());
+                Thread.sleep(1000);
+               } catch (InterruptedException e) {
+                //TODO Auto-generated catch block
+        
+                e.printStackTrace();
+        }
+        
+            }
+    //return hora;
+   }
 
 }
