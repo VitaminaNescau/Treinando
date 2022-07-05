f947a1acd821015e35d64aa5d41ee82b147bb425
VitaminaNescau <vitorjss82@gmail.com>
2022-07-05 12:44:37 -0300

    deleted

--- Java/sampleFullpratic.java
+++
@@ -1,8 +0,0 @@
-package Java;
-
-public class sampleFullpratic {
-    //nesse arquivo tentaria por em pratica thread/exception/inteface
-    public static void main(String[] args) {
-        System.out.println("teste");
-    }
-}
