027564f60cb019b33ca5e6294892aecbd9371313
VitaminaNescau <vitorjss82@gmail.com>
2022-07-07 01:01:40 -0300

    update

--- Java/relogio/hours.java
+++ Java/relogio/hours.java
@@ -1,5 +1,51 @@
 package Java.relogio;
 
-public class hours {
-    
+import java.util.Calendar;
+
+public class hours implements Runnable{
+    private int alarm;
+    private Calendar time;
+    public int hr,mm,sc;
+    public String hours;
+    public hours(int wake){
+        this.alarm = wake;
+    }
+    public hours(){
+
+    }
+    public void run(){
+        hours teste = new hours();
+        while (true) {
+        try {  System.out.println(teste.watch());
+        
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        
+    }
+
+    public String watch(){
+        time = Calendar.getInstance();
+        hr = time.get(Calendar.HOUR_OF_DAY);
+        mm = time.get(Calendar.MINUTE);
+        sc = time.get(Calendar.SECOND);
+        this.hours = (hr+":"+mm);
+        return hours;
+    }
+
+    public void getAlarm(){
+        if (alarm == Integer.parseInt(hours)) {
+            System.out.println("tocou");
+        }
+        //return alarm;
+    }
+
+
+
+
+
 }
+
--- Java/relogio/watch.java
+++ Java/relogio/watch.java
@@ -1,5 +1,11 @@
 package Java.relogio;
 
+import javax.sound.midi.Track;
+
 public class watch {
-    
+    public static void main(String[] args) {
+        hours thread = new hours();
+        Thread t1 = new Thread(thread);
+        t1.start();
+    }
 }
