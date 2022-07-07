c4e249befa42c8099fe688344eca02340ce141e2
VitaminaNescau <vitorjss82@gmail.com>
2022-07-07 20:09:27 -0300

    Alarm fix

--- Java/relogio/hours.java
+++ Java/relogio/hours.java
@@ -3,44 +3,44 @@ package Java.relogio;
 import java.util.Calendar;
 
 public class hours implements Runnable{
-    private int alarm;
+    private String alarm;
     private Calendar time;
     public int hr,mm,sc;
     public String hours;
-    public hours(int wake){
-        this.alarm = wake;
-    }
+    /* public hours(int wake){
+        this.alarm = wake;    
+    } */
     public hours(){
-
     }
     public void run(){
-        hours teste = new hours();
+        hours clock = new hours();
         while (true) {
-        try {  System.out.println(teste.watch());
-        
+        try {  System.out.println(clock.watch());
                 Thread.sleep(1000);
+                clock.getAlarm(alarm);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+          
         }
         
     }
-
     public String watch(){
         time = Calendar.getInstance();
         hr = time.get(Calendar.HOUR_OF_DAY);
         mm = time.get(Calendar.MINUTE);
         sc = time.get(Calendar.SECOND);
-        this.hours = (hr+":"+mm);
+        this.hours = (hr+":"+mm+":"+sc);
         return hours;
     }
 
-    public void getAlarm(){
-        if (alarm == Integer.parseInt(hours)) {
+    public void getAlarm( String setAlarm){
+        this.alarm = setAlarm;
+        //System.out.println(alarm +"  "+hr);
+        if (alarm.equals(hours)) {
             System.out.println("tocou");
         }
-        //return alarm;
+       
     }
 
 
