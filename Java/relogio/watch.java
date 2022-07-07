d6177429f765071d2a4327fecb9f206233a41908
VitaminaNescau <vitorjss82@gmail.com>
2022-07-07 20:09:43 -0300

    Interface

--- Java/relogio/watch.java
+++ Java/relogio/watch.java
@@ -1,11 +1,14 @@
 package Java.relogio;
 
-import javax.sound.midi.Track;
 
 public class watch {
     public static void main(String[] args) {
         hours thread = new hours();
         Thread t1 = new Thread(thread);
         t1.start();
+       String getAlarm = "20:0:20";
+        thread.getAlarm(getAlarm);
+        // hours thread2 = new hours( teste = 3);
+       
     }
 }
