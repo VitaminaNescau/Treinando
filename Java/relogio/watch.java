cbc217cbf17e9ecf174ed8c952fb474350267d57
VitaminaNescau <vitorjss82@gmail.com>
2022-07-08 14:40:41 -0300

    watch fix

--- Java/relogio/watch.java
+++ Java/relogio/watch.java
@@ -1,13 +1,33 @@
 package Java.relogio;
 
+import java.util.Scanner;
 
 public class watch {
     public static void main(String[] args) {
         hours thread = new hours();
         Thread t1 = new Thread(thread);
         t1.start();
-       String getAlarm = "20:0:20";
-        thread.getAlarm(getAlarm);
+        String getAlarm = null;
+        //String resposta = null;
+        while (true) {
+            System.out.println("Relogio\n1-Alarme\n2-Sair");
+            int resposta = new Scanner(System.in).nextInt();
+            if (resposta == 1) {
+                System.out.print("Hora:");
+                String teste;
+                getAlarm = teste = new Scanner(System.in).next();
+                thread.getAlarm(getAlarm); 
+                
+            } else {
+                break;
+            }    
+       }
+       
+       
+        
+
+       
+       
         // hours thread2 = new hours( teste = 3);
        
     }
