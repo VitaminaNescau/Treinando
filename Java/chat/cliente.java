5ad238e2011d8e7e19e90b04da5da30001814abf
VitaminaNescau <vitorjss82@gmail.com>
2022-07-23 20:19:46 -0300

    modified:   Java/chat/cliente.java

--- Java/chat/cliente.java
+++ Java/chat/cliente.java
@@ -20,41 +20,42 @@ public class cliente {
         cS = new Socket("127.0.0.1", 3030);
         out = new PrintWriter(cS.getOutputStream());
         in = new BufferedReader(new InputStreamReader(cS.getInputStream()));
+        Thread sender = new Thread(new Runnable() {
+            String msg;
+            @Override
+            public void run(){
+             while(true){
+                 msg = sc.nextLine();
+                 out.println(msg);
+                 out.flush();
+             }
+            } 
+         });
+         sender.start();
+         Thread receive = new Thread(new Runnable() {
+             String msg;
+             @Override
+             public void run() {
+                 try {
+                     msg = in.readLine();
+                     while (msg!=null) {
+                         System.out.println("Servidor: " + msg);
+                         msg = in.readLine();
+                     }
+                     System.out.println("Servido fora do ar");
+                     out.close();
+                     cS.close();
+                 } catch (Exception e) {
+                     System.out.println(e);
+                 }
+                 
+             }
+         });
+         receive.start();
     } catch (IOException e) {
        System.out.println(e);
     }
-    Thread sender = new Thread(new Runnable() {
-       String msg;
-       @Override
-       public void run(){
-        while(true){
-            msg = sc.nextLine();
-            out.println(msg);
-            out.flush();
-        }
-       } 
-    });
-    sender.start();
-    Thread receive = new Thread(new Runnable() {
-        String msg;
-        @Override
-        public void run() {
-            try {
-                msg = in.readLine();
-                while (msg!=null) {
-                    System.out.println("Servidor: " + msg);
-                    msg = in.readLine();
-                }
-                System.out.println("Servido fora do ar");
-                out.close();
-                cS.close();
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-            
-        }
-    });
-    receive.start();
+   
 
 }
 
