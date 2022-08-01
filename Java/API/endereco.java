51b66509481b202cada73436c3167af425ba9f46
VitaminaNescau <vitorjss82@gmail.com>
2022-08-01 10:51:58 -0300

    class com os atributos q requisitamos

---
+++ Java/API/endereco.java
@@ -0,0 +1,22 @@
+package API;
+
+public class endereco {
+
+    String logradouro;
+    String bairro;
+    String localidade;
+
+    public  String getLogradouro(){
+        return logradouro;
+    }
+    public  String getBairro(){
+        return bairro;
+    }
+    public  String getLocalidade(){
+        return localidade;
+    }
+    @Override
+    public String toString() {
+        return "Endereco{" + "logradouro=" + logradouro + ", bairro=" + bairro + ", localidade=" + localidade + '}';
+    }
+}
