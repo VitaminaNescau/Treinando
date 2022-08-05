91e260d8b46c01336985e9c9a77600075f1f9a6d
VitaminaNescau <vitorjss82@gmail.com>
2022-08-04 23:10:51 -0300

    Correção e simplificação da interface

--- Java/relogio/watch.java
+++ Java/relogio/watch.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class watch {
     public static void main(String[] args) {
-        hours teste = new hours();
-        teste.horas();
+        hours watch = new hours();
+        watch.horas();
         while (true) {
             System.out.println("Relogio\n1-Alarme\n2-Sair");
             int resposta = new Scanner(System.in).nextInt();
             if (resposta == 1) {
                 System.out.print("Hora:");
                 String alarm = new Scanner(System.in).nextLine();
-                teste.getAlarm(alarm);
-                
-                
-            } else {
+                watch.getAlarm(alarm);  
+            } else if(resposta == 2) {
                 break;
-            }    
+            }  else{
+                System.out.println("Comando Invalido");
+            }
        }
        
        
