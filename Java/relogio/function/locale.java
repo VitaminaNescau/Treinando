e19bd154bc826f128208c75516dc98abe5b5317c
VitaminaNescau <vitorjss82@gmail.com>
2022-08-08 13:40:11 -0300

    atributos da api

---
+++ Java/relogio/function/locale.java
@@ -0,0 +1,12 @@
+package relogio.function;
+
+public class locale {
+    private String cid;
+
+    public void setCity(String cid){
+        this.cid = cid;
+    }
+    public String getCity(){
+        return cid;
+    }
+}
