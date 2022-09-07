be295408ac78e4d7fe5434cae6ada6ea13b7618d
VitaminaNescau <vitorjss82@gmail.com>
2022-09-06 23:13:02 -0300

    atv 3

---
+++ Senai/atividades3/atv.java
@@ -0,0 +1,40 @@
+package Senai.atividades3;
+
+public class atv {
+    static double[] fibo = new double[21]; 
+
+    public static void main(String[] args) {
+        //usando função pra fazer fibo
+        //2 formas de usar metodospara fibonanci
+      System.out.println("metodo void");
+        getFibo();
+        System.out.println("metodo com retorno obrigatorio");
+        for (double teste : fibo()) {
+        System.out.println(teste);
+    }
+    }
+    public static void getFibo(){
+       int x,y,i=0;
+       x=1;
+       y=0;
+       while (i<=20) {
+        x=x+y;
+       y=x-y;
+       i++;
+       System.out.println(x);
+       }
+     
+    } 
+    public static double[] fibo(){
+        int x,y,i=0;
+       x=1;
+       y=0;
+        while (i<=20) {
+            x=x+y;
+           y=x-y;
+          fibo[i] = x;
+           i++;
+        
+        }
+        return fibo;
+    }}
