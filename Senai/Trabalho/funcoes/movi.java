359750f5b1534573f686447904d0446350d594a6
VitaminaNescau <vitorjss82@gmail.com>
2022-10-03 01:02:59 -0300

    33%

--- Senai/Trabalho/funcoes/movi.java
+++ Senai/Trabalho/funcoes/movi.java
@@ -3,17 +3,18 @@ package Senai.Trabalho.funcoes;
 import java.util.Scanner;
 
 import Senai.Trabalho.membros.f1;
+import Senai.Trabalho.membros.family;
 
 public class movi{
-    f1 vitor;
+    family user;
     
     public void movimenta(){
         
         System.out.println("1 - Saques\n2 - Deposito");
         int t = new Scanner(System.in).nextInt();
         switch (t) {
-            case 1:
-                System.out.print("Saldo da conta:");
+            case 1://to chegando perto
+                System.out.print("Saldo da conta:"+ user.vitor.getSaldo());
                 break;
             case 2: 
                 break;
